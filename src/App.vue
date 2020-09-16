756987a10f00ba4f0b81cde1f415174493679303
Steve Doggett <steve@orbitalwitness.com>
2020-09-16 10:33:14 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <app-roll-the-dice  />
+</template>
+
+<script>
+import RollTheDice from './components/RollTheDice.vue'
+
+export default {
+  name: 'App',
+  
+    components: {
+    appRollTheDice: RollTheDice,
+  },
+}
+</script>
---
+++ src/components/RollTheDice.vue
@@ -0,0 +1,49 @@
+﻿<template>
+    <h1>Number is: {{ dice }}</h1>
+    <div>Number of rolls: {{ rolls.length }}</div>
+    <div>Total: {{ total }}</div>
+    <button @click="roll()">Let's roll the dice</button>
+    <button @click="restart()">Restart</button>
+    <ul>
+        <li v-for="(t, index) in rolls" :key="index">
+            {{ t }}
+        </li>
+    </ul>
+</template>
+
+<script>
+    import { computed, ref } from 'vue'
+
+    export default {
+        name: 'RollTheDice',
+
+        setup() {
+            const dice = ref(0)
+            const rolls = ref([])
+
+            function roll() {
+                dice.value = Math.floor(Math.random() * Math.floor(5)) + 1
+                rolls.value.unshift(dice.value)
+            }
+
+            function restart() {
+                dice.value = 0
+                rolls.value = []
+            }
+
+            const total = computed(() =>
+                rolls.value.reduce((accumulator, val) => {
+                    return accumulator + val
+                }, 0))
+
+            return {
+                dice,
+                rolls,
+                total,
+                roll,
+                restart,
+            }
+        }
+    }
+</script>
+
